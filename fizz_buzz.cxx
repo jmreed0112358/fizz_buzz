908059714a442c8ea14f1be0d6b3dccaa2e3a027
Jeremy Reed <reeje76@gmail.com>
2011-05-15 09:13:42 +0000

    Modified fizz_buzz to take four arguments, two numbers to test for divisiblity, and a min, max range for testing.
    git-svn-id: file:///home/jmreed/files/svn/ouoroboros/apps/fizz_buzz@92 038c2593-57d5-4b29-bb80-940b0eb19d4b

--- fizz_buzz.cxx
+++ fizz_buzz.cxx
@@ -1,19 +1,46 @@
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
-int main( void )
+int main( int argc, char* argv[] )
 {
 
    int i = 0;
+   int f = 0;
+   int g = 0;
    int x = 0;
    int y = 0;
+   int min = 0;
+   int max = 0;
 
-   for ( i = 1 ; i != 101 ; i++ )
+   if ( argc != 5 )
+   {
+      cout << "usage: fizz_buzz <x> <y> <min> <max>" << endl
+           << "x, y: Numbers to check for divisiblity" << endl
+           << "min, max: Range of numbers to test against." << endl << endl;
+      exit(-1);
+   }
+   else
+   {
+      cout << "x:   " << argv[1] << endl
+           << "y:   " << argv[2] << endl
+           << "min: " << argv[3] << endl
+           << "max: " << argv[4] << endl << endl;
+   }
+
+   f = atoi( argv[1] );
+   g = atoi( argv[2] );
+   min = atoi( argv[3] );
+   max = atoi( argv[4] );
+
+   for ( i = min ; i != max ; i++ )
    {
 
-      x = i % 3;
-      y = i % 5;
+      x = i % f;
+      y = i % g;
+
+      
 
       if ( ( x == 0 ) && ( y == 0 ) )
       {
