d54c7747866e37edef7fc05d1ebadf5fe26dc88a
Jeremy M. Reed <reeje76@gmail.com>
2013-05-23 10:43:58 -0400

    check_input: Added const to function definition.
    Makes things a bit more consistent, having const in the function definition as it is in the function declaration.

--- fizz_buzz.h
+++ fizz_buzz.h
@@ -33,7 +33,8 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
    int process_input( const char* F, const char* G, 
                       const char* MIN, const char* MAX,
                       long int* f, long int* g, long int* min, long int* max );
-   int check_input( long int f, long int g, long int min, long int max );
+   int check_input( const long int f, const long int g, 
+                    const long int min, const long int max );
    void print_usage( );
    
 
